7dc9a948e2a8aed1723fd3eaf6342fd57bf95875
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-11-26 22:31:43 +0200

    navbar folder

---
+++ src/components/navbar/layout/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResponsiveNavBar from '../nav/Navbar';
+
+const NavbarLayout = () => {
+  return (
+    <React.Fragment>
+      <ResponsiveNavBar />
+    </React.Fragment>
+  );
+};
+
+export default NavbarLayout;
