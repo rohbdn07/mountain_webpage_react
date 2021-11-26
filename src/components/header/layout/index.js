066fcd8ad0b055c69d0a4d76fb496d533e1abf71
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-11-26 22:30:47 +0200

    styling with css and material ui

--- src/components/header/layout/index.js
+++ src/components/header/layout/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import Container from '@mui/material/Container';
 import CompanyName from '../title/CompanyName';
+import Logo from '../logo/Logo';
+import './styles.css';
 
 const HeaderLayout = () => {
   return (
     <React.Fragment>
-      <div>
-        <CompanyName />
-      </div>
+      <Container maxWidth="xl">
+        <div className="headerlayout_container">
+          <div className="headerlayout_banner">
+            <div className="banner_left">
+              <Logo />
+              <CompanyName />
+            </div>
+            <div className="banner_right">
+              <Logo />
+            </div>
+          </div>
+        </div>
+      </Container>
     </React.Fragment>
   );
 };
