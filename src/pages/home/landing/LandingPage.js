3a8bd9e2433cf5e39035ae9fed0fb7f594516a5c
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-11-26 22:32:26 +0200

    code refactor and styling

--- src/pages/home/landing/LandingPage.js
+++ src/pages/home/landing/LandingPage.js
@@ -1,27 +1,22 @@
+import { Container } from '@mui/material';
 import React from 'react';
 import HeaderLayout from '../../../components/header/layout';
+import NavbarLayout from '../../../components/navbar/layout';
+import './styles.css';
 
 const LandingPage = () => {
   return (
     <React.Fragment>
-      <div className="landing_page">
-        <div className="landing_page_header">
-          <HeaderLayout />
-        </div>
-        <div className="landing_page_body">
-          <div className="landing_page_body_content">
-            <div className="landing_page_body_content_title">
-              <h1>Mountain Ski Resort</h1>
-              <p>A simple web application for the mountain climbing community</p>
-            </div>
-            <div className="landing_page_body_content_links">
-              <a href="/">Home</a>
-              <a href="/about">About</a>
-              <a href="/contact">Contact</a>
-            </div>
+      <Container>
+        <div className="landing_page">
+          <div className="landing_page_header">
+            <HeaderLayout />
+          </div>
+          <div>
+            <NavbarLayout />
           </div>
         </div>
-      </div>
+      </Container>
     </React.Fragment>
   );
 };
