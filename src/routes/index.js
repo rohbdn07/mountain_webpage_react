a832e7c8a50941d1186cf5cb4a051d4c79b6645b
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-11-26 22:29:34 +0200

    routes for pages are created

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,10 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from '../pages/home/landing/LandingPage';
 
 const AppRoutes = () => {
+  console.log('AppRoutes');
   return (
     <Routes>
-      <Route path="/" element={LandingPage} />
+      <Route path="/" element={<LandingPage />} />
     </Routes>
   );
 };
