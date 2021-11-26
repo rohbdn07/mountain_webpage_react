db0b488097cdbba2fa573daac09015ee35086b57
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-11-26 22:54:40 +0200

    address component

--- src/components/header/layout/index.js
+++ src/components/header/layout/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import CompanyName from '../title/CompanyName';
 import Logo from '../logo/Logo';
+import PinDropIcon from '@mui/icons-material/PinDrop';
 import './styles.css';
+import Address from '../location/Address';
 
 const HeaderLayout = () => {
   return (
@@ -15,7 +17,8 @@ const HeaderLayout = () => {
               <CompanyName />
             </div>
             <div className="banner_right">
-              <Logo />
+              <PinDropIcon />
+              <Address />
             </div>
           </div>
         </div>
---
+++ src/components/header/location/Address.js
@@ -0,0 +1,18 @@
+import Box from '@mui/material/Box';
+
+const Address = () => {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: 20,
+        fontSize: 10,
+        opacity: [0.9, 0.8, 0.7]
+      }}>
+      <p>Hel-Fin Street</p>
+      <p>0-4251</p>
+    </Box>
+  );
+};
+
+export default Address;
